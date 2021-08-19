fdc955874bee92a589470d6103a41fa8cdcf663c
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2021-08-19 11:17:00 -0600

    libreria, consola de pruebas y estructura inicial de nodo

---
+++ ConsolaDePruebas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Libreria_ED2;
+namespace ConsolaDePruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Libreria_ED2/ArbolB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libreria_ED2
+{
+    public class ArbolB<T> where T : IComparable
+    {
+        private class Nodo
+        {
+            public T[] datos;
+            public Nodo[] hijos;
+            public Nodo Padre;
+            int grado;
+
+            public Nodo(int _grado)
+            {
+                grado = _grado;
+                datos = new T[_grado];
+                hijos = new Nodo[_grado + 1];
+            }
+        }
+    }
+}
