a41e5c641c90f62ae4f9017045d1881740fee56b
Luis Escobar <luis982210@gmail.com>
2021-08-29 13:36:55 -0600

    Eliminar arbol b

--- Libreria_ED2/ArbolB.cs
+++ Libreria_ED2/ArbolB.cs
@@ -372,13 +372,355 @@ namespace Libreria_ED2
 
 
         }
-        public void EliminarArbol()
+        public void eliminar(T dato)
         {
-            Raiz = null;
+            if (Raiz == null)
+            {
+                // no haga nada
+            }
+            else
+            {
+                Nodo buscarEliminar = Raiz;
+                bool existe = BuscarValor(ref buscarEliminar, dato);
+
+                if (existe == false)
+                {
+                    // no existe el dato no haga nada
+                }
+                else
+                {
+                    int posicion = PosicionEliminar(ref buscarEliminar, dato);
+                    if (buscarEliminar.hijos[0] != null)
+                    {
+                        Nodo buscarEliminarPosAnterior = buscarEliminar;
+                        int posicionSubir = BuscarNodoMasGrandeSubArbol(ref buscarEliminar, dato, posicion);
+                        buscarEliminarPosAnterior.datos[posicion] = buscarEliminar.datos[posicionSubir];
+                        buscarEliminar.datos[posicionSubir] = default;
+                    }
+                    else
+                    {
+                        for (int i = posicion; i < grado - 1; i++)
+                        {
+                            buscarEliminar.datos[i] = buscarEliminar.datos[i + 1];
+                        }
+
+                    }
+
+                    while (EqualityComparer<T>.Default.Equals(buscarEliminar.datos[((grado - 1) / 2) - 1], default) == true && buscarEliminar != Raiz)
+                    {
+
+                        bool sePresto = PedirPrestadoHermano(ref buscarEliminar, dato);
+
+                        if (sePresto == false)
+                        {
+                            FusionarNodos(ref buscarEliminar, dato);
+                        }
+                    }
+
+                    if (buscarEliminar == Raiz && EqualityComparer<T>.Default.Equals(buscarEliminar.datos[0], default) == true && buscarEliminar.hijos[0] != null)
+                    {
+                        Raiz = Raiz.hijos[0];
+                    }
+                    else if (EqualityComparer<T>.Default.Equals(buscarEliminar.datos[0], default) == true)
+                    {
+                        Raiz = null;
+                    }
+
+
+
+                }
+            }
+
+        }
+        #region Auxiliares eliminar
+        private int PosicionEliminar(ref Nodo buscarEliminar, T dato)
+        {
+            int posicion = 0;
+            for (int i = 0; i < grado; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(buscarEliminar.datos[i], default) == false)
+                {
+                    if (dato.CompareTo(buscarEliminar.datos[i]) == 0)
+                    {
+                        posicion = i;
+                    }
+                }
+            }
+            return posicion;
+        }
+        #endregion
+        private int BuscarNodoMasGrandeSubArbol(ref Nodo buscarEliminar, T dato, int posicion)
+        {
+            buscarEliminar = buscarEliminar.hijos[posicion];
+            int posicionHoja = 0;
+            bool encontrado;
+            int iterador;
+            while (buscarEliminar.hijos[0] != null)
+            {
+                encontrado = false;
+                iterador = grado;
+                while (encontrado == false)
+                {
+                    if (buscarEliminar.hijos[iterador] != null)
+                    {
+                        buscarEliminar = buscarEliminar.hijos[iterador];
+                        encontrado = true;
+                    }
+                    else
+                    {
+                        iterador--;
+                    }
+                }
+            }
+
+            encontrado = false;
+            iterador = grado - 1;
+            while (encontrado == false)
+            {
+                if (EqualityComparer<T>.Default.Equals(buscarEliminar.datos[iterador], default) == false)
+                {
+                    posicionHoja = iterador;
+                    encontrado = true;
+                }
+                else
+                {
+                    iterador--;
+                }
+            }
+            return posicionHoja;
         }
 
+        private bool PedirPrestadoHermano(ref Nodo buscarEliminar, T dato)
+        {
+            Nodo padreAux = buscarEliminar.padre;
+            int posicionRaizComun = 0;
+            bool raizComunEncontrada = false;
+            int iterador = 0;
+            while (raizComunEncontrada == false)
+            {
+                if (EqualityComparer<T>.Default.Equals(padreAux.datos[iterador], default) == false)
+                {
+                    if (dato.CompareTo(padreAux.datos[iterador]) == -1)
+                    {
+                        raizComunEncontrada = true;
+                        posicionRaizComun = iterador;
+                    }
+                    else
+                    {
+                        iterador++;
+                    }
+                }
+                else
+                {
+                    raizComunEncontrada = true;
+                    posicionRaizComun = iterador;
+                }
+            }
+
+            bool sePrestoValor = false;
+
+            if ((posicionRaizComun - 1) >= 0)
+            {
+                if (EqualityComparer<T>.Default.Equals(padreAux.hijos[posicionRaizComun - 1].datos[((grado - 1) / 2)], default) == false)
+                {
+
+                    for (int i = grado - 1; i > 0; i--)
+                    {
+                        buscarEliminar.datos[i] = buscarEliminar.datos[i - 1];
+                    }
+                    for (int i = grado; i > 0; i--)
+                    {
+                        buscarEliminar.hijos[i] = buscarEliminar.hijos[i - 1];
+                    }
+
+                    buscarEliminar.datos[0] = padreAux.datos[posicionRaizComun - 1];
+
+                    bool valorMayor = false;
+                    iterador = grado - 1;
+
+                    while (valorMayor == false)
+                    {
+                        if (EqualityComparer<T>.Default.Equals(padreAux.hijos[posicionRaizComun - 1].datos[iterador], default) == false)
+                        {
+                            valorMayor = true;
+                            padreAux.datos[posicionRaizComun - 1] = padreAux.hijos[posicionRaizComun - 1].datos[iterador];
+                            padreAux.hijos[posicionRaizComun - 1].datos[iterador] = default;
+                            sePrestoValor = true;
+                            if (padreAux.hijos[posicionRaizComun - 1].hijos[iterador + 1] != null)
+                            {
+                                buscarEliminar.hijos[0] = padreAux.hijos[posicionRaizComun - 1].hijos[iterador + 1];
+                                buscarEliminar.hijos[0].padre = buscarEliminar;
+                                padreAux.hijos[posicionRaizComun - 1].hijos[iterador + 1] = default;
+                            }
+                        }
+                        else
+                        {
+                            iterador--;
+                        }
+                    }
+                }
+            }
+            if (sePrestoValor == false && padreAux.hijos[posicionRaizComun + 1] != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(padreAux.hijos[posicionRaizComun + 1].datos[((grado - 1) / 2)], default) == false)
+                {
+
+                    bool posicionVaciaEncontrada = false;
+                    int posicionBajarRaiz = 0;
+                    iterador = 0;
+                    while (posicionVaciaEncontrada == false)
+                    {
+                        if (EqualityComparer<T>.Default.Equals(buscarEliminar.datos[iterador], default) == true)
+                        {
+                            posicionBajarRaiz = iterador;
+                            posicionVaciaEncontrada = true;
+                        }
+                        else
+                        {
+                            iterador++;
+                        }
+                    }
+
+                    buscarEliminar.datos[posicionBajarRaiz] = padreAux.datos[posicionRaizComun];
+                    padreAux.datos[posicionRaizComun] = padreAux.hijos[posicionRaizComun + 1].datos[0];
+                    if (padreAux.hijos[posicionRaizComun + 1].hijos[0] != null)
+                    {
+                        buscarEliminar.hijos[posicionBajarRaiz + 1] = padreAux.hijos[posicionRaizComun + 1].hijos[0];
+                        padreAux.hijos[posicionRaizComun + 1].hijos[0].padre = buscarEliminar;
+                        padreAux.hijos[posicionRaizComun + 1].hijos[0] = default;
 
 
+                    }
 
+                    bool valorMayor = false;
+
+                    iterador = grado - 1;
+
+                    for (int i = 0; i < grado - 1; i++)
+                    {
+                        padreAux.hijos[posicionRaizComun + 1].datos[i] = padreAux.hijos[posicionRaizComun + 1].datos[i + 1];
+                    }
+                    for (int i = 0; i < grado; i++)
+                    {
+                        padreAux.hijos[posicionRaizComun + 1].hijos[i] = padreAux.hijos[posicionRaizComun + 1].hijos[i + 1];
+                    }
+                    sePrestoValor = true;
+                }
+
+            }
+
+            return sePrestoValor;
+        }
+
+        private void FusionarNodos(ref Nodo buscarEliminar, T dato)
+        {
+            Nodo padreAux = buscarEliminar.padre;
+            int posicionRaizComun = 0;
+            bool raizComunEncontrada = false;
+            int iterador = 0;
+            while (raizComunEncontrada == false)
+            {
+                if (EqualityComparer<T>.Default.Equals(padreAux.datos[iterador], default) == false)
+                {
+                    if (dato.CompareTo(padreAux.datos[iterador]) == -1)
+                    {
+                        raizComunEncontrada = true;
+                        posicionRaizComun = iterador;
+                    }
+                    else
+                    {
+                        iterador++;
+                    }
+                }
+                else
+                {
+                    raizComunEncontrada = true;
+                    posicionRaizComun = iterador;
+                }
+            }
+
+            bool seDividio = false;
+            int posMinimaSiguiente = (grado - 1) / 2;
+            if ((posicionRaizComun - 1) >= 0)
+            {
+                // bajar la raiz izquierda
+                padreAux.hijos[posicionRaizComun - 1].datos[posMinimaSiguiente] = padreAux.datos[posicionRaizComun - 1];
+                int j = 0;
+                //pasar valores a nodo izquierdo
+                for (int i = posMinimaSiguiente + 1; i < grado - 1; i++)
+                {
+                    padreAux.hijos[posicionRaizComun - 1].datos[i] = padreAux.hijos[posicionRaizComun].datos[j];
+                    j++;
+                }
+                j = 0;
+                for (int i = posMinimaSiguiente + 1; i < grado; i++)
+                {
+                    padreAux.hijos[posicionRaizComun - 1].hijos[i] = padreAux.hijos[posicionRaizComun].hijos[j];
+                    if (padreAux.hijos[posicionRaizComun].hijos[j] != null)
+                    {
+                        padreAux.hijos[posicionRaizComun].hijos[j].padre = padreAux.hijos[posicionRaizComun - 1];
+                    }
+                    j++;
+                }
+                //cambiar apuntador 
+                padreAux.hijos[posicionRaizComun] = padreAux.hijos[posicionRaizComun - 1];
+                //arrastrar valroes
+                for (int i = posicionRaizComun - 1; i < grado - 1; i++)
+                {
+                    padreAux.datos[i] = padreAux.datos[i + 1];
+                }
+                for (int i = posicionRaizComun; i < grado; i++)
+                {
+                    padreAux.hijos[i] = padreAux.hijos[i + 1];
+                }
+
+                seDividio = true;
+                buscarEliminar = padreAux;
+
+            }
+            if (seDividio == false && posicionRaizComun + 1 < grado - 1)
+            {
+                padreAux.hijos[posicionRaizComun].datos[posMinimaSiguiente - 1] = padreAux.datos[posicionRaizComun];
+                iterador = 0;
+
+                for (int i = posMinimaSiguiente; i < grado - 1; i++)
+                {
+                    padreAux.hijos[posicionRaizComun].datos[i] = padreAux.hijos[posicionRaizComun + 1].datos[iterador];
+                    iterador++;
+                }
+                iterador = 0;
+                for (int i = posMinimaSiguiente; i < grado; i++)
+                {
+                    padreAux.hijos[posicionRaizComun].hijos[i] = padreAux.hijos[posicionRaizComun + 1].hijos[iterador];
+                    if (padreAux.hijos[posicionRaizComun].hijos[iterador] != null)
+                    {
+                        padreAux.hijos[posicionRaizComun].hijos[iterador].padre = padreAux.hijos[posicionRaizComun];
+                    }
+                    iterador++;
+                }
+
+                padreAux.hijos[posicionRaizComun + 1] = padreAux.hijos[posicionRaizComun];
+
+                for (int i = posicionRaizComun; i < grado - 1; i++)
+                {
+                    padreAux.datos[i] = padreAux.datos[i + 1];
+                }
+
+                for (int i = posicionRaizComun; i < grado; i++)
+                {
+                    padreAux.hijos[i] = padreAux.hijos[i + 1];
+                }
+
+                buscarEliminar = padreAux;
+
+            }
+
+
+        }
     }
+
+
+
+
 }
+
